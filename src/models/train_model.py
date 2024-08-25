180e43b958b4dc85f489354f1904c9368e4d9208
nigiledwin <nigilke9416@gmail.com>
2024-08-25 21:51:33 +0200

    test 13

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.ensemble import RandomForestRegressor
@@ -18,7 +18,7 @@ import dagshub
 #initiate dagshub server
 # Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("DAGSHUB_PAT")
-print(f"DAGSHUB_PAT is set to: {dagshub_token[:10]}...")
+
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 print(f"DAGSHUB_PAT is set to: {dagshub_token[:10]}...")
@@ -72,29 +72,29 @@ def preprocessing(X_train, y_train, X_test, y_test):
         
         y_pred = pipe.predict(X_test.iloc[:1, :])
         y_true = y_test.iloc[:1].values.ravel()
-        rmse = mean_squared_error(y_true, y_pred, squared=False)  # RMSE calculation
+        rmse = root_mean_squared_error(y_true, y_pred)  # RMSE calculation
 
         
         # Get feature names after transformation
         feature_names = get_feature_names(trf1, original_feature_names)
 
-        # SHAP explainability
+        """# SHAP explainability
         # Convert the transformed data to a dense format
         X_train_transformed = pipe.named_steps['trf1'].transform(X_train).toarray()
         X_test_transformed = pipe.named_steps['trf1'].transform(X_test.iloc[:1, :]).toarray()
 
         explainer = shap.Explainer(pipe.named_steps['model'], X_train_transformed)
-        shap_values = explainer(X_test_transformed)
+        shap_values = explainer(X_test_transformed)"""
 
         # Ensure the 'models' directory exists
         os.makedirs('models', exist_ok=True)
 
-        # Save SHAP values for further analysis
+        """# Save SHAP values for further analysis
         np.save('models/shap_values.npy', shap_values.values)
         #shap.summary_plot(shap_values, X_test_transformed, show=False)
         shap.waterfall_plot(shap.Explanation(values=shap_values[0].values, base_values=shap_values[0].base_values, data=X_test_transformed[0], feature_names=feature_names), show=False)
         #plt.savefig('models/shap_summary_plot.png')
-        plt.savefig('models/shap_waterfall.png')
+        plt.savefig('models/shap_waterfall.png')"""
 
         #log mlflow experiments
         mlflow.log_metric('acuracy',rmse)
